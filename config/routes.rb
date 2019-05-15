38ff9d7347db5eeefd55fdd369f87b6c25a0e359
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2019-05-15 15:47:32 +0200

    completes api setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'repositories', to: 'search#respositories'
+  namespace :api do
+    namespace :v1 do
+      get 'repositories', to: 'search#repositories'
+    end
+  end
 end
